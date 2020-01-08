61894b22f8d6b3530631e919d71a6d8cddcbe639
Admin <Admin@172.26.219.98>
2020-01-08 12:17:02 +0100

    Primeros cambios

---
+++ Compartir/src/compartir/Compartir.java
@@ -0,0 +1,16 @@
+
+package compartir;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Compartir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
